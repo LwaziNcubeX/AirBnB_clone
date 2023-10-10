84fde2ecb2f783d6af9f7ecff3600603f6f3967e
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-10 23:40:35 +0200

    Modified BaseModel's to_dict() method to only convert date to isoformat if necessary

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        self.__dict__['__class__'] = self.__class__.__name__
-        return self.__dict__
+        new_dict = self.__dict__.copy()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
