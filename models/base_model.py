7a30c390029d2b4d7be97e8ddb9bc799e7181e8a
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-11 00:31:33 +0200

    Re-create an instance with this dictionary representation.

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,18 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -21,4 +29,5 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         new_dict['__class__'] = self.__class__.__name__
+        new_dict['id'] = self.id
         return new_dict
