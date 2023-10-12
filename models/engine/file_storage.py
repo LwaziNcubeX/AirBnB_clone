b7208cb61af7a5c5c13c010d30c263fbe742777b
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-12 23:22:39 +0200

    Updated save function to append new data instead of overwritting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,10 @@
 """A class FileStorage that works with JSON"""
 import json
 
-from models.base_model import BaseModel
 
-
-class FileStorage(BaseModel):
+class FileStorage:
     __file_path = 'file.json'
     __objects = {}
-    id = 12121212
 
     def all(self):
         return self.__objects
@@ -18,17 +15,27 @@ class FileStorage(BaseModel):
         self.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, 'w') as f:
-            json.dump([obj.to_dict() for obj in self.__objects.values()], f)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = FileStorage.__objects
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
+                obj_dict = json.load(f)
+        except FileNotFoundError:
+            pass
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(obj_dict, f)
 
     def reload(self):
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
                 objects = json.load(f)
                 for obj in objects:
-                    cls_name = obj.pop('__class__')
-                    cls = globals()[cls_name]
-                    key = "{}.{}".format(cls_name, obj['id'])
-                    self.__objects[key] = cls(**obj)
+                    if isinstance(obj, dict):
+                        cls_name = obj.pop('__class__')
+                        cls = globals()[cls_name]
+                        key = "{}.{}".format(cls_name, obj['id'])
+                        self.__objects[key] = cls(**obj)
         except FileNotFoundError:
             pass
