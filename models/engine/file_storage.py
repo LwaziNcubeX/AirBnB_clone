9a4d637c05db1a023d0d0c5ec535e9fb30da9356
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-13 00:58:47 +0200

    Fixed the reload func to print reloaded objs when required

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A class FileStorage that works with JSON"""
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -8,34 +9,31 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
         obj_dict = FileStorage.__objects
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
                 obj_dict = json.load(f)
         except FileNotFoundError:
             pass
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w', encoding='utf-8') as f:
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
                 objects = json.load(f)
-                for obj in objects:
-                    if isinstance(obj, dict):
-                        cls_name = obj.pop('__class__')
-                        cls = globals()[cls_name]
-                        key = "{}.{}".format(cls_name, obj['id'])
-                        self.__objects[key] = cls(**obj)
+                for obj in objects.values():
+                    cls_name = obj.pop("__class__")
+                    self.new(globals()[cls_name](**obj))
         except FileNotFoundError:
-            pass
+            return
