ba30d8eb15cd0e56b65791ba1cf1db3eff17d2e6
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-13 00:59:02 +0200

    Fixed the reload func to print reloaded objs when required

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A class BaseModel that defines all common attributes/methods"""
 from datetime import datetime
-from models import storage
+import models
 import uuid
 
 
@@ -18,14 +18,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         new_dict = self.__dict__.copy()
